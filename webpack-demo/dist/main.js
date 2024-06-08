88d9dc60d14497cb4fcbdbc78fe28653415ead96
murraygi <133593977+murraygi@users.noreply.github.com>
2024-06-08 12:15:13 +0100

    additional js tests in webpack

--- webpack-demo/dist/main.js
+++ webpack-demo/dist/main.js
@@ -26,7 +26,29 @@ eval("/* module decorator */ module = __webpack_require__.nmd(module);\nvar __WE
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\n\n function component() {\n   const element = document.createElement('div');\n\n  // Lodash, now imported by this script\n   element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Hello', 'webpack'], ' ');\n\n   return element;\n }\n\n document.body.appendChild(component());\n\n//# sourceURL=webpack://webpack-demo/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _myName__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./myName */ \"./src/myName.js\");\n/* harmony import */ var _myModule__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./myModule */ \"./src/myModule.js\");\n\n\n \n\n function component() {\n   const element = document.createElement('div');\n\n  // Lodash, now imported by this script\n   element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Hello', 'webpack'], ' ');\n\n  // Adding a newline for better readability\n  element.innerHTML += '<br>';\n\n  // Using your function to add more content\n  element.innerHTML += (0,_myName__WEBPACK_IMPORTED_MODULE_1__[\"default\"])('Cody');\n\n   return element;\n }\n\n document.body.appendChild(component());\n\n//# sourceURL=webpack://webpack-demo/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/myModule.js":
+/*!*************************!*\
+  !*** ./src/myModule.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   functionOne: () => (/* binding */ functionOne),\n/* harmony export */   functionTwo: () => (/* binding */ functionTwo)\n/* harmony export */ });\nconst functionOne = () => 'ONE';\nconst functionTwo = () => 'TWO';\n\n\n\n//# sourceURL=webpack://webpack-demo/./src/myModule.js?");
+
+/***/ }),
+
+/***/ "./src/myName.js":
+/*!***********************!*\
+  !*** ./src/myName.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst myName = (name) => 'Hi! My name is ' + name;\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (myName);\n\n//# sourceURL=webpack://webpack-demo/./src/myName.js?");
 
 /***/ })
 
--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import myName from './myName';
+import {functionOne, functionTwo} from './myModule'; 
 
  function component() {
    const element = document.createElement('div');
 
   // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  // Adding a newline for better readability
+  element.innerHTML += '<br>';
+
+  // Using your function to add more content
+  element.innerHTML += myName('Cody');
 
    return element;
  }
